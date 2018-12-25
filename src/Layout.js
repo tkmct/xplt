309cf4566cdfe0d2b4acc1359cbdafc0b62b5630
tkmct <1220t.takamichi@gmail.com>
2018-12-25 17:34:17 +0900

    Add fade animation to layout

--- src/Layout.js
+++ src/Layout.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import { View, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native'
 
-export default ({ children, onNext, onPrevious }) => <TouchableWithoutFeedback onPress={onNext} onLongPress={onPrevious}>
-  <View style={style.container}>{children}</View>
-</TouchableWithoutFeedback>
+export default class Layout extends React.Component {
+  state = {
+    fadeAnimation: new Animated.Value(0),
+  }
+
+  componentDidMount() {
+    Animated.timing(
+      this.state.fadeAnimation,
+      {
+        toValue: 1,
+        duration: 500,
+      }
+    ).start();
+  }
+
+  render () {
+    const { children, onNext, onPrevious } = this.props;
+    const { fadeAnimation } = this.state;
+
+    return <TouchableWithoutFeedback onPress={onNext} onLongPress={onPrevious}>
+      <Animated.View style={{ ...style.container, opacity: fadeAnimation }}>{children}</Animated.View>
+    </TouchableWithoutFeedback>
+  }
+} 
 
 const style = StyleSheet.create({
   container: {
